5eb065fbc2a0cb026dd7765d7609f473511a8c08
Ricard Luquero <rluquero@claudator.com>
2012-04-12 21:56:15 +0200

    Creación de los bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Cupon\OfertaBundle\OfertaBundle(),
+            new Cupon\CiudadBundle\CiudadBundle(),
+            new Cupon\TiendaBundle\TiendaBundle(),
+            new Cupon\UsuarioBundle\UsuarioBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
