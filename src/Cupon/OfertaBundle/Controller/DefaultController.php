aaeb155975e63bfb101b6d04a829b5c66b44ae05
Ricard <rluquero@gmail.com>
2012-06-21 18:23:07 +0200

    Avances portada

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -5,10 +5,21 @@ namespace Cupon\OfertaBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-	public function ayudaAction()
-	{
-		return $this->render('OfertaBundle:Default:ayuda.html.twig');
-	}
+class DefaultController extends Controller {
+
+    public function ayudaAction() {
+	return $this->render('OfertaBundle:Default:ayuda.html.twig');
+    }
+
+    public function portadaAction() {
+	$em = $this->getDoctrine()->getEntityManager();
+	$oferta = $em->getRepository('OfertaBundle:Oferta')->findOneBy(array(
+	    'ciudad' => 26
+		));
+
+	return $this->render(
+			'OfertaBundle:Default:portada.html.twig', array('oferta' => $oferta)
+	);
+    }
+
 }
